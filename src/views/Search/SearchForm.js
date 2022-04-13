b133cf186a08e0bc00578e9004818abbb9778066
Lenifc <lenifc@outlook.vom>
2022-04-13 18:32:13 +0200

    created form and results page

--- src/views/Search/SearchForm.js
+++ src/views/Search/SearchForm.js
@@ -1,12 +1,149 @@
 import SearchResults from './SearchResults'
 
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext'
+import { Card } from 'primereact/card'
+
+import { toast } from 'react-toastify'
+import { useState, useEffect } from 'react'
+
+import firebase from 'firebase/app'
+import validPhoneNum from '../../components/validPhoneNum.js'
+import validateVIN from '../../components/validateVIN.js'
+
+import { useContext } from 'react'
+import ContextStore from '../../store/ContextStore'
+
 function SearchForm(){
 
+    const [searchNumber, setSearchNumber] = useState('')
+    const [searchVIN, setSearchVIN] = useState('')
+    const [outputClient, setOutputClient] = useState('')
+    const [outputVehicles, setOutputVehicles] = useState('')
 
-    return(<>
-    SearchForm
-    <SearchResults />
-    </>)
+    const { activeSearchResults, getActiveSearchResults} = useContext(ContextStore)
+
+
+    function validSearchData(target) {
+
+        let buttons = document.querySelectorAll('.searchBtn')
+        let vinBtn = document.querySelector('.vinBtn')
+        let phoneBtn = document.querySelector('.phoneBtn')
+      
+        buttons.forEach(btn => btn.classList.remove('active'))
+        try{
+            if (target === 'PhoneNumber' && searchNumber.trim()) {
+                let legitPhoneNum = validPhoneNum(searchNumber.trim())
+                if (legitPhoneNum) {
+                  phoneBtn.classList.add('active')
+                  searchInFirestore(legitPhoneNum, 'phoneNum')
+                }
+                else throw Error('Phone number format is invalid')
+            }
+            if (target === 'VIN' && searchVIN.trim()) {
+                let VINNumber = validateVIN(searchVIN)
+                if (VINNumber) {
+                    vinBtn.classList.add('active')
+                    searchInFirestore(VINNumber, 'VIN')
+                } 
+                else throw Error('Check VIN format')
+            }
+        } 
+        catch(err){
+            toast.warn('Invalid input - ' + err.message)
+        }
+    }
+      
+
+    function searchInFirestore(searchData, searchOption) {
+        setOutputVehicles('')
+        setOutputClient('')
+      
+        if (searchOption === 'VIN') searchForVehicles(searchData)
+        if (searchOption === 'phoneNum') searchForClient(searchData)
+    }
+    
+
+    async function searchForClient(data, order){
+        const clients = firebase.firestore()
+                                .collection('warsztat')
+                                .doc('Klienci').collection("Numery").doc(data)
+      
+        const response = (await clients.get()).data()
+      
+        if(response) {
+            setOutputClient(response)
+            getActiveSearchResults([response, outputVehicles])
+      
+            if(order !== 2) await searchForVehicles(response.Tel, 2)
+        }
+        else {
+            if(order !== 2) toast.warn(`Cannot find client with provided phone number.`)
+        }
+    }
+      
+      
+    async function searchForVehicles(data, order){
+        let vehicle
+
+        try{
+            if(order && order === 2) vehicle = firebase.firestore()
+                                                .collection('warsztat')
+                                                .doc('Pojazdy').collection("VIN").where('Tel', '==', data) 
+            else vehicle = firebase.firestore()
+                                    .collection('warsztat')
+                                    .doc('Pojazdy').collection("VIN").doc(data)
+          
+            let response      
+            order && order === 2 ? response = await vehicle.get() : response = (await vehicle.get()).data()
+          
+            if(response){
+                order && order === 2 ? response = response.docs?.map(doc => doc.data()) : response = [response]
+          
+                setOutputVehicles(response)
+                getActiveSearchResults([outputClient, response])
+          
+                if(response?.[0]?.Tel) await searchForClient(response[0].Tel, 2)
+            }
+            else if(!order) throw Error(`Cannot find vehicle with provided VIN.`)
+        } catch(err){
+            toast.warn(err.message)
+        }
+    }
+      
+    useEffect(() =>{
+        if(activeSearchResults) {
+            setOutputClient(activeSearchResults[0])
+            setOutputVehicles(activeSearchResults[1])
+        }
+    }, [])
+
+
+    const cardVehicleTitle = 'Search for vehicle by VIN'
+    const cardVehicleFooter = <Button onClick={() => validSearchData('VIN')} icon="pi pi-search" className="p-button-primary searchBtn vinBtn" 
+                                        label="Search" id="vin" />
+
+    const cardClientTitle = 'Search for client by phone number'
+    const cardClientFooter = <Button onClick={() => validSearchData('PhoneNumber')} icon="pi pi-search" className="p-button-primary searchBtn phoneBtn" 
+                                        label="Search"/>
+
+    return(
+        <div>
+        <div className="flex flex-column md:flex-row justify-content-center pt-5 text-center">
+          <Card className="mx-0 md:mx-3 my-3 md:my-0" title={cardVehicleTitle} footer={cardVehicleFooter}>
+              <label htmlFor="searchByVIN"></label>
+              <InputText name="searchClient" id="searchByVIN" value={searchVIN} onChange={(e) => setSearchVIN(e.target.value)} maxLength="17" style={{width: 'min(100%, 200px)'}}
+                tooltip="Valid VIN length is 17-digits" />
+          </Card>
+          <Card className="mx-0 md:mx-3 my-3 md:my-0" title={cardClientTitle} footer={cardClientFooter}>
+              <label htmlFor="searchByPhoneNum"></label>
+              <InputText name="searchClient" id="searchByPhoneNum" value={searchNumber} onChange={(e) => setSearchNumber(e.target.value)} style={{width: 'min(100%, 200px)'}}
+                tooltip="7-digits for landline ; 9-digits for mobile" label="Search for client"/>
+          </Card>
+        </div>
+      { (outputClient?.length > 0 || outputVehicles?.length > 0) && <SearchResults outputVehicles={outputVehicles} outputClient={outputClient} className="pt-5"/>}
+      
+      </div>)
 }
 
 export default SearchForm
--- src/views/Search/SearchResults.js
+++ src/views/Search/SearchResults.js
@@ -1,9 +1,196 @@
-function SearchResults(){
+import './SearchResults.css'
+
+import { Button } from 'primereact/button'
+import { Card } from 'primereact/card'
+import { Divider } from 'primereact/divider'
+import { confirmDialog } from 'primereact/confirmdialog'
+import { toast } from 'react-toastify'
+
+import { DeleteFunc } from '../../components/EditMoveDeleteOptions'
+
+import { callTicketsHistory } from '../../components/fetchTicketHistory'
+import TicketsHistory from '../Tasks/TicketsHistory'
+
+import ContextStore from '../../store/ContextStore'
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import firebase from 'firebase/app'
 
 
-    return(<>
-    Results
-    </>)
+function SearchResults({outputVehicles, outputClient}){
+
+    const navigate = useNavigate()
+
+    const { activeSearchResults, getActiveSearchResults, getClientData, getVehicleData} = useContext(ContextStore)
+
+    const [resultData, setResultData] = useState('')
+    const [client, setClient] = useState(activeSearchResults ? activeSearchResults[0] : '')
+
+
+
+    const clientPath = firebase.firestore()
+                                .collection('warsztat').doc('Klienci').collection('Numery')
+    const vehiclePath = firebase.firestore()
+                                .collection('warsztat').doc('Pojazdy').collection('VIN')
+
+    function openEditClientForm(client){
+        getClientData(client)
+        navigate(`/clients/details/${client?.['Tel']}/edit`)
+    }
+
+    function openEditVehicleForm(car){
+        getVehicleData(car)
+        navigate(`/vehicles/details/${car['VIN']}/edit`)
+    }
+
+    function openCarDetails(car){
+        getVehicleData(car)
+        navigate(`/vehicles/details/${car['VIN']}`)
+    }
+
+    function openClientDetails(client){
+        getClientData(client)
+        navigate(`/clients/details/${client?.['Tel']}`)
+    }
+
+    const confirmDeleteModal = async (operation, target) => {
+      const checkForTickets = (await callTicketsHistory(target || 0)).some(ticket => ticket[1].length > 0)
+
+    confirmDialog({
+        message: operation === 'removeClient' ?
+          `Are you sure you want to delete client: ${target}?` : 
+          (operation === 'removeCar' ? 
+            `Are you sure you want to delete vehicle: ${target}?` : 'Invalid data type!'),
+        header: operation === 'removeClient' ?
+          `Delete client` : (operation === 'removeCar' ? `Delete vehicle` : 'Invalid data type!'),
+        icon: 'pi pi-exclamation-triangle',
+        acceptClassName: 'p-button-success',
+        rejectClassName: 'p-button-danger',
+        acceptLabel: 'Tak',
+        rejectLabel: 'Nie',
+        accept: async () => {
+            if (operation === 'removeClient') {
+                const confirmDelete = await DeleteFunc('Client', clientPath, target)
+                if (confirmDelete !== false) setResultData(['', resultData[1]])
+            }
+            if (operation === 'removeCar') {
+                if(checkForTickets === false){
+                    const confirmDelete = await DeleteFunc('Vehicle', vehiclePath, target)
+                    if (confirmDelete !== false) {
+                        let filteredVehicle = resultData[1].filter(car => car.VIN !== target)
+                        setResultData([resultData[0], filteredVehicle])
+                    } 
+                    else toast.info('Cannot fully delete vehicle, because it contains service history')
+                } 
+                else toast.warn('Cannot fully delete vehicle, because it contains service history')
+            }
+        },
+        reject: () => {}
+      });
+    }
+
+
+    useEffect(() => {
+      if(!outputClient && !outputVehicles){
+        setResultData(activeSearchResults)
+      } else setResultData([outputClient, outputVehicles])
+        setClient(outputClient || activeSearchResults)
+    }, [])
+
+    // workaround for client that is not loading properly when searching by VIN
+    useEffect(() =>{
+        if(!outputClient && !outputVehicles){
+            setResultData(activeSearchResults)
+        } 
+        else setResultData([outputClient, outputVehicles])
+            setClient(outputClient || activeSearchResults)
+    }, [outputClient])
+
+    useEffect(() => {
+        setClient(resultData[0])
+    },[resultData])
+
+    const cardHeader = <>
+        { !client && <h3 className="text-center pt-4" >Vehicle does not have assigned client</h3> } 
+        { client && <div>
+            <div className="text-center pt-3">
+              <h3>Client:</h3>
+            </div>
+            <div className="toolbarClient">
+              <Button className="p-button-danger mr-2 p-button-rounded" icon="pi pi-trash" tooltip="Delete client" onClick={() => confirmDeleteModal('removeClient', client.Tel)} />
+              <Button className="p-button-primary p-button-rounded" icon="pi pi-pencil" tooltip="Edit client" onClick={() => openEditClientForm(client)} />
+            </div>
+        </div>}
+    </>
+
+
+    return(
+        <div className="flex flex-column justify-content-center align-items-center">
+            { resultData && resultData.length && <>
+            <div className="flex flex-row justify-content-center pt-4">
+                <Button icon="pi pi-ban" className="p-button-secondary" label="Clear results" onClick={() => {
+                        getActiveSearchResults('')
+                        setResultData('')
+                        document.querySelectorAll('.searchBtn').forEach(btn => btn.classList.remove('active'))
+                }} />
+            </div>
+            <h3 className="pt-6 text-center"> Results:</h3>
+            <div className="pb-4 pt-3" style={{width:'100%', maxWidth:'1000px'}}>
+                <Card className="relative" header={cardHeader}>
+                    <div className="client">
+                        <div className="my-1">
+                            { client?.Imie && <span className="font-bold">Name:</span>} {client?.['Imie']}
+                        </div>
+                        <div className="my-1">
+                            { client?.Tel && <span className="font-bold">Contact number: </span>} 
+                            <span className="font-bold mouse-pointer link" onClick={() => openClientDetails(client)}>{client?.['Tel']}</span>
+                        </div>
+                        { client?.Tel2 && <div className="my-1">
+                            <span className="font-bold">2nd contact number:</span> { client?.['Tel2']}
+                        </div>}
+                        { client?.kodPocztowy && client?.Miejscowosc && client?.Ulica && <div>
+                            <div className="my-1"><span className="font-bold">{ client?.['kodPocztowy'] }</span> { client?.['Miejscowosc'] }</div>
+                            <div className="my-1"><span className="font-bold"></span> { client?.['Ulica']} Street</div>
+                        </div>}
+                        { client?.Opis && <div className="my-1">
+                            <span className="font-bold">Description:</span> <span dangerouslySetInnerHTML={{__html: client?.['Opis']}}></span>
+                        </div>}
+                </div>
+                    <Divider />
+                <div className="vehicles">
+        
+                    { resultData?.[1] && <h3 className="text-center">Client's vehicles:</h3>}
+                    { !resultData?.[1] && <h3>Client does not have any assigned vehicles</h3>}
+                    {resultData?.[1].map(vehicle => {
+                    return ( <div className="vehicle relative" key={vehicle.VIN}>
+                        <div className="toolbarVehicle">
+                            <Button className="p-button-danger mr-2 p-button-rounded" icon="pi pi-trash" tooltip="Delete vehicle" onClick={() => confirmDeleteModal('removeCar', vehicle.VIN)} />
+                            <Button className="p-button-primary p-button-rounded" icon="pi pi-pencil" tooltip="Edit vehicle data" onClick={() => openEditVehicleForm(vehicle)} />
+                        </div>
+                        <div className="my-1 font-bold">{ `${vehicle['Marka']} ${vehicle['Model']} ${vehicle['Wersja_Rocznik'] || ''}`}</div>
+                        <div className="my-1">
+                            <span className="font-bold">VIN: </span> 
+                            <span className="font-bold mouse-pointer link" onClick={() => openCarDetails(vehicle)}>{vehicle['VIN']}</span> 
+                        </div>
+                        { vehicle['Numer_rejestracyjny'] && <div className="my-1"><span className="font-bold">Licence plate:</span> {vehicle['Numer_rejestracyjny']}</div>}
+                        <div className="my-1"><span className="font-bold">Fuel type:</span> {vehicle['Paliwo'].name || vehicle['Paliwo']}</div>
+                        { vehicle['Przebieg'] && <div className="my-1"><span className="font-bold">Mileage:</span> {vehicle['Przebieg']} km</div>}
+                        { vehicle['Silnik_Pojemnosc'] && <div className="my-1"><span className="font-bold">Engine capacity:</span> {vehicle['Silnik_Pojemnosc']} cm<sup>3</sup></div>}
+                        { vehicle['Silnik_Moc'] && <div className="my-1"><span className="font-bold">Power/Torque:</span> {vehicle['Silnik_Moc']} KM</div>}
+                        { vehicle['Silnik_Kod'] && <div className="my-1"><span className="font-bold">Engine code:</span> {vehicle['Silnik_Kod']}</div>}
+                        { vehicle['Naped'] && <div className="my-1"><span className="font-bold">Drivetrain type:</span> {vehicle['Naped']}</div>}
+                        { vehicle['SkrzyniaBiegow'] && <div className="my-1"><span className="font-bold">Transmission:</span> {vehicle['SkrzyniaBiegow']}</div>}
+                        <TicketsHistory VIN={vehicle?.['VIN']} Tel={client?.['Tel']} />
+                        <Divider />
+                    </div>)
+                    })}
+                </div>
+            </Card>
+            </div>
+            </>}
+            </div>
+    )
 }
 
 export default SearchResults
